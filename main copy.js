cac9869cc465b04f16a8cafe278f9f8e8ba974bc
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2025-08-22 14:34:12 +0200

    Clap de fin prix affiché

--- main copy.js
+++ main copy.js
@@ -1,114 +1,108 @@
 // Assignation de l'eventListener sur CHAQUE bouton radio
-var radios = $("input[name='pain']");
-$(radios).each(function() {
-    $(this).change(function() {
+var radios = document.sandwich_maker.pain;
+for (var i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('change', function() {
         generateRecipe();
     });
-});
+}
 // Assignation de l'eventListener sur CHAQUE checkbox
-var checkboxes = $("input[name='ingredients[]']");
-$(checkboxes).each(function() {
-    $(this).change(function() {
+var checkboxes = document.getElementsByName('ingredients[]');
+for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', function() {
         generateRecipe();
     });
-});
+}
 // Renvoie la valeur du bouton radio "pain" qui a été choisi
 function getValuePain() {
     var value = '';
-    var radios = $("input[name='pain']");
-    $(radios).each(function() {
-        if($(this).prop('checked') == true) {
-            value = $(this).val();
+    var radios = document.sandwich_maker.pain;
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            value = radios[i].value;
         }
-    });
+    }
     return value;
 }
 // Renvoie la ou les valeurs sélectionnées dans les checkboxes ingrédients
 function getValueIngredients() {
     var values = [];
-    var checkboxes = $('input[name="ingredients[]"]:checked');
-    $(checkboxes).each(function() {
-        values.push($(this));
-    });
+    var checkboxes = document.getElementsByName('ingredients[]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            values.push(checkboxes[i].value);
+        }
+    }
     return values;
 }
 // Renvoie la valeur du select "sauce" qui a été choisi
 function getValueSauce() {
-    var value = $('#sauce').val();
-    var prix = $('#sauce option:selected').data('prix');
-    var arrayReturn = [];
-    arrayReturn['texte'] = value;
-    arrayReturn['prix'] = prix;
-    return arrayReturn;
+    var value = document.getElementById('sauce').value;
+    return value;
 }
 // Renvoie la valeur du select "sauce" qui a été choisi
 function getValueInstruction() {
-    var value = $('#instruction').val();
+    var value = document.getElementById('instruction').value;
     return value;
 }
 // Génération de la recette
 function generateRecipe() {
-    var total = 0;
     var recette = '';
+
     // Gestion du pain
     var pain = getValuePain();
     if(pain != '') {
-        total += +$("input[name='pain']:checked").data('prix');
         recette += "Vous avez choisi le pain : " + pain;
     }
     // Gestion des ingredients
     var ingredients = getValueIngredients();
     if(ingredients.length > 0) {
         recette += "<br>avec les ingrédients suivants : <ul>";
-        $(ingredients).each(function() {
-            total += +$(this).data('prix');
-            recette += "<li>" + $(this).val() + "</li>";
-        });
+        for(i = 0; i < ingredients.length; i++) {
+            recette += "<li>" + ingredients[i] + "</li>";
+        }
         recette += "</ul>";
     }
     // Gestion de la sauce
     var sauce = getValueSauce();
-    if(sauce['texte'] != '') {
-        total += +sauce['prix'];
-        recette += "<br>Votre sauce est : " + sauce['texte'];
+    if(sauce != '') {
+        recette += "<br>Votre sauce est : " + sauce;
     }
     // Gestion de l'instruction spéciale
     var instruction = getValueInstruction();
     if(instruction != '') {
         recette += "<br>Votre instruction est : " + instruction;
     }
-    $('#recette').html(recette);
-    $('#total').html("<div><p class='text-center'>Votre total : </p><div id='prix'>" + (Math.round(total * 100) / 100).toFixed(2) + "€</div></div>");
+    document.getElementById('recette').innerHTML = recette;
 }
 function shuffle() {
     // Choix aléatoire d'un bouton radio (un seul)
-    var radios = $("input[name='pain']");
+    var radios = document.sandwich_maker.pain;
     var randomRadioIndex = Math.floor(Math.random() * (radios.length - 1));
-    $(radios).each(function(i) {
+    for (var i = 0; i < radios.length; i++) {
         if(i == randomRadioIndex) {
-            $(this).prop('checked', true);
+            radios[i].checked = true;
         }
-    });
+    }
     // Choix aléatoire d'une ou de plusieurs cases à cocher
-    var checkboxes = $("input[name='ingredients[]']");
+    var checkboxes = document.getElementsByName('ingredients[]');
     // Ici on décoche toutes les cases pour avoir une base de travail saine
-    $(checkboxes).each(function() {
-        $(this).prop('checked', false);
-    });
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
     // On met les checkboxes dans un tableau
     var arrayCheckboxes = [];
-    $(checkboxes).each(function() {
-        arrayCheckboxes.push($(this));
-    });
+    for(i=0; i < checkboxes.length; i++) {
+        arrayCheckboxes.push(checkboxes[i]);
+    } 
     // Et on mélange le tableau pour obtenir le coté aléatoire
     melangerTableau(arrayCheckboxes);
     // Puis on prend un nombre au hasard pour trancher le tableau en deux et ne garder qu'une moitié
     var randomCheckboxIndex = Math.floor(Math.random() * (checkboxes.length - 1));
     arrayCheckboxes.splice(0, randomCheckboxIndex);
     // Et enfin on fait une boucle pour passer toutes nos checkboxes restantes à "cochée"
-    $(arrayCheckboxes).each(function() {
-        $(this).prop('checked', true);
-    });
+    for(i = 0; i < arrayCheckboxes.length; i++) {
+        arrayCheckboxes[i].checked = true;
+    }
     // Gestion de l'aléatoire sur le select
     var select = document.getElementById('sauce');
     var items = select.getElementsByTagName('option');
@@ -118,17 +112,20 @@ function shuffle() {
 }
 function melangerTableau(array) {
   let currentIndex = array.length;
+
   // Tant qu'il nous reste des éléments
   while (currentIndex != 0) {
+
     // On choisit un nombre au hasard entre 0 et la longueur du tableau
     let randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
+
     // Et on échange la position de l'index en cours avec le chiffre aléatoire 
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex], array[currentIndex]];
   }
 }
 function showFormulaire() {
-    $("#maRow").removeClass("d-none");
-    $("#showFormulaire").addClass("d-none");
+    document.getElementById("maRow").classList.remove("d-none");
+    document.getElementById("showFormulaire").classList.add("d-none");
 }
